0d76728cd293ef912716a68bdb357c8da78c61f9
CleveyP <clevelandplonsey@gmail.com>
2023-07-23 03:45:14 -0700

    create view controller routes folder structure

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const UsersModel = require("../Models/Users");
+
+
+const register = async (req, res) =>{
+    //get the username and passowrd from request object
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check the database to make sure that the username is available
+    const result = await UsersModel.findOne({username: username}).exec();
+    if(result){
+        res.send({success: false, message: "username is already taken"});
+    }
+    // if we get here, username is available
+    //hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    //add the new user to the database
+      try {
+    let newUser = await UsersModel.create({
+      username: username,
+      password: hash
+    });
+    console.log(newUser);
+    console.log("created new user: " + username);
+    res.send({success: true, message: "Added new user " + username});
+  } catch (err) {
+    console.log(err);
+    res.send({success: false, message: err});
+  }
+}
+
+const login =  async (req, res) =>{
+    //get the username and password from request object
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check the database to see if this credential combo matches
+    const result = await UsersModel.findOne({username: username});
+    console.log(result);
+    //get the hashed password from that user
+    const dbHash = result.password;
+    //if the password is correct 
+    if(bcrypt.compareSync(password, dbHash )){
+        res.send({success: true, username: username});
+    }
+    else{
+        res.send({success: false, username: "not logged in"});
+    }
+
+       
+}
+
+module.exports = {register, login}
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const userRouter = express.Router();
+const {register, login} = require("../controllers/userControllers");
+
+userRouter.post("/login", login);
+
+userRouter.post("/register", register);
+
+module.exports = userRouter;
+
+
+module.exports = userRouter;
